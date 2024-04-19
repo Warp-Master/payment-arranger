0040e9cc56c9f9ec64441789798e9af100ae49ae
Dmitrii Chernov <2003dima@gmail.com>
2024-04-19 19:04:28 +0300

    feat: add argparser

--- README.md
+++ README.md
@@ -4,8 +4,12 @@
 3. Install dependencies (`pip install -r requirements.txt`)
 
 # Usage
-1. put your .xslx files next to main.py file
+1. put your `*.xslx` files next to `main.py` file (can be changed with --inp argument)
 2. Run script using venv `./venv/bin/python main.py`
-3. log will be saved to `payment.log`
+3. log will be saved to `payment.log` (can be changed with --log argument)
+4. Subdirectories with files will be created next to `main.py` file (can be changed with --dst argument)
 
 use CRON job or alternative to create shedule for this task
+
+you can use `./venv/bin/python main.py --help` for additional info
+
--- main.py
+++ main.py
@@ -1,38 +1,85 @@
-import openpyxl
-import glob
-import os
-import warnings
+import argparse
 import logging
+import os
 import re
+import shutil
+import warnings
 from pathlib import Path
 
-
-# disable warnings from openpyxl
-warnings.filterwarnings("ignore", category=UserWarning, module=re.escape('openpyxl.styles.stylesheet'))
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.FileHandler("payment.log"),
-        logging.StreamHandler()
-    ]
-)
-
-for filename in glob.glob('*.xlsx'):
-    # read payment info
-    wb = openpyxl.load_workbook(filename, data_only=True)
-    ws = wb.active
-    inn = ws["D21"].value
-    reciever = ws["B22"].value
-    purpose = ws["B27"].value
-    amount = ws["T11"].value
-    logging.info(f"[New payment] to={reciever} for={purpose} ({amount})")
-    wb.close()
-
-    # move file to new location
-    dir = Path(inn)
-    os.makedirs(dir, exist_ok=True)
-    os.rename(filename, dir / filename)
+import openpyxl
 
 
+
+def dir_path(path) -> Path:
+    if os.path.isdir(path):
+        return Path(path)
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
+
+
+def build_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n",
+        "--dry-run",
+        dest="is_dry_run",
+        action="store_true",
+        help="disable file movement and subdirectory creation",
+    )
+    parser.add_argument(
+        "-l", "--log", help="specify log file", type=Path, default="payment.log"
+    )
+    parser.add_argument(
+        "-i",
+        "--inp",
+        type=dir_path,
+        default=Path("."),
+        help="directory with .xlsx files",
+    )
+    parser.add_argument(
+        "-d",
+        "--dst",
+        type=Path,
+        default=Path("."),
+        help="directory where subdirectories with files will be created",
+    )
+    return parser
+
+
+def configure_logging(args) -> None:
+    # disable warnings about style from openpyxl
+    warnings.filterwarnings(
+        "ignore", category=UserWarning, module=re.escape("openpyxl.styles.stylesheet")
+    )
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[logging.FileHandler(args.log), logging.StreamHandler()],
+    )
+
+
+def main(args=None) -> None:
+    argparser = build_argparser()
+    args = argparser.parse_args(args)
+    configure_logging(args)
+    for filename in args.inp.glob("*.xlsx"):
+        # read payment info
+        wb = openpyxl.load_workbook(filename, data_only=True, read_only=True)
+        ws = wb.active
+        inn = ws["D21"].value
+        reciever = ws["B22"].value
+        purpose = ws["B27"].value
+        amount = ws["T11"].value
+        logging.info(f"[payment] to={reciever} for={purpose} ({amount})")
+        wb.close()
+
+        if not args.is_dry_run:
+            # move file to new location
+            dir = args.dst / inn
+            os.makedirs(dir, exist_ok=True)
+            shutil.move(filename, dir / filename.name)
+
+
+if __name__ == "__main__":
+    main()
